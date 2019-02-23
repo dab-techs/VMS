05c1e7f56948a95728b18e78548e32c7d55cdef8
dab-techs <42230434+dab-techs@users.noreply.github.com>
2019-02-23 13:33:23 +0530

    Add files via upload

---
+++ footerForAll.php
@@ -0,0 +1,17 @@
+<?php {?>
+
+<link rel="stylesheet" type="text/css" href="footer.css"> 
+
+
+	<div class = "footer">
+		
+	<span > 2019 © Charotar University of Science and Technology 
+	</span>
+
+
+	</div>
+
+
+
+
+<?php }?>
